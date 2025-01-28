8927d24b2c5595f4b15041d9ac1bbcebbeedc4e1
Austin Ewell <austin.ewell86@gmail.com>
2025-01-28 13:18:56 -0700

    added auto router and started refactoring from view to page folder format

---
+++ src/pages/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go To Home</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
