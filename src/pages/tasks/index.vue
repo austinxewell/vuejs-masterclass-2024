82315aa54a773965bdb2230f0aef9ac7e00eec73
Austin Ewell <austin.ewell86@gmail.com>
2025-01-30 10:59:19 -0700

    updated project[slug] to display project specifics

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { columns } from '@/utils/tableColumns/tasksColumns'
 import { tasksWithProjectsQuery } from '@/utils/supaQueries'
-import type { tasksWithProjects } from '@/utils/supaQueries'
+import type { TasksWithProjects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'My Tasks'
 
-const tasks = ref<tasksWithProjects | null>(null)
+const tasks = ref<TasksWithProjects | null>(null)
 
 const getTasks = async () => {
   const { data, error } = await tasksWithProjectsQuery
