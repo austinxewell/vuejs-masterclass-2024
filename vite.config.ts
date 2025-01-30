9f5f21bf05da24e5bcc94dca4409f196cfd47009
Austin Ewell <austin.ewell86@gmail.com>
2025-01-29 19:36:14 -0700

    added and refactored code to use auto-import-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // Import Tailwind CSS and Autoprefixer as ES modules
@@ -30,10 +32,11 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: true,
       viteOptimizeDeps: true,
     }),
+    Components({}),
   ],
   css: {
     postcss: {
