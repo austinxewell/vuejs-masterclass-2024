8bf3b95002501ee7ccba17857ea822519cdce7c8
Austin Ewell <austin.ewell86@gmail.com>
2025-01-31 09:11:24 -0700

    created error type and store to display more accurate error messages

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
+
+useErrorStore().setError({ error: 'Page not Found', customCode: 404 })
 </script>
 
 <template>
