dcaeef17c1d00a038ae97f28817181ac8d39cfb4
Austin Ewell <austin.ewell86@gmail.com>
2025-02-05 20:38:50 -0700

    added form kit to project

---
+++ src/components/AppNew/AppNewTask.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { FormKit } from '@formkit/vue'
+
+const sheetOpen = defineModel<boolean>()
+</script>
+
+<template>
+  <Sheet v-model:open="sheetOpen">
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Create New Task</SheetTitle>
+      </SheetHeader>
+
+      <FormKit type="text" />
+    </SheetContent>
+  </Sheet>
+</template>
