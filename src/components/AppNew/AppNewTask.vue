e722c24ecac48fde51264a9cb460fd43a7641c73
Austin Ewell <austin.ewell86@gmail.com>
2025-02-05 20:52:13 -0700

    added dummy form and created form type

--- src/components/AppNew/AppNewTask.vue
+++ src/components/AppNew/AppNewTask.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import type { CreateNewTask } from '@/types/CreateNewForm'
 import { FormKit } from '@formkit/vue'
 
 const sheetOpen = defineModel<boolean>()
+
+const createTask = async (formData: CreateNewTask) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(console.log(formData))
+    }, 2000)
+  })
+}
 </script>
 
 <template>
@@ -11,7 +20,30 @@ const sheetOpen = defineModel<boolean>()
         <SheetTitle>Create New Task</SheetTitle>
       </SheetHeader>
 
-      <FormKit type="text" />
+      <FormKit type="form" @submit="createTask" submit-label="Create Task">
+        <FormKit label="Name" placeholder="My New Task" />
+        <FormKit
+          type="select"
+          name="for"
+          label="For"
+          placeholder="Select a User"
+          :options="[{ label: 'Item name', value: 1 }]"
+        />
+        <FormKit
+          type="textarea"
+          name="description"
+          label="Description"
+          placeholder="Task Description"
+        />
+        <FormKit
+          type="select"
+          name="project"
+          id="project"
+          label="Project"
+          placeholder="Select a Project"
+          :options="[{ label: 'Item name', value: 1 }]"
+        />
+      </FormKit>
     </SheetContent>
   </Sheet>
 </template>
