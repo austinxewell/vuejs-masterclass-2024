ea54d0570a568547cedd5f26ef8930aa6eae7635
Austin Ewell <austin.ewell86@gmail.com>
2025-02-08 02:24:23 -0700

    lets try this again.

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -2,10 +2,6 @@
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
 
 useErrorStore().setError({ error: 'Page not Found', customCode: 404 })
-
-useMeta({
-  title: 'Error | Pulse',
-})
 </script>
 
 <template>
