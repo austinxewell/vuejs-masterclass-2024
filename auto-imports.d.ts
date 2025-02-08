8fd949464e1d1f42a1f43f2b5b2884f541f80c05
Austin Ewell <austin.ewell86@gmail.com>
2025-02-08 02:20:19 -0700

    trying to fix deploy

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -65,7 +65,6 @@ declare global {
   const useFormErrors: typeof import('./src/composables/formErrors')['useFormErrors']
   const useId: typeof import('vue')['useId']
   const useLink: (typeof import('vue-router'))['useLink']
-  const useMeta: typeof import('vue-meta')['useMeta']
   const useModel: typeof import('vue')['useModel']
   const usePageStore: typeof import('./src/stores/page')['usePageStore']
   const useProjectsStore: typeof import('./src/stores/loaders/projects')['useProjectsStore']
