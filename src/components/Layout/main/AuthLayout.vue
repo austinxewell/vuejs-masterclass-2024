be999a7d0a932cfef1f7b720d06ae3e500cb0124
Austin Ewell <austin.ewell86@gmail.com>
2025-02-07 20:14:20 -0700

    added transition on page navigation and login/logout

--- src/components/Layout/main/AuthLayout.vue
+++ src/components/Layout/main/AuthLayout.vue
@@ -15,16 +15,21 @@ provide(menuKey, {
 </script>
 
 <template>
-  <AppSidebar @task-clicked="taskSheetOpen = true" />
-  <AppNewTask v-model="taskSheetOpen" />
-  <div class="flex flex-col transition-[margin]" :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }">
-    <TopNavbar />
+  <div>
+    <AppSidebar @task-clicked="taskSheetOpen = true" />
+    <AppNewTask v-model="taskSheetOpen" />
+    <div
+      class="flex flex-col transition-[margin]"
+      :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }"
+    >
+      <TopNavbar />
 
-    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
-      </div>
-      <slot />
-    </main>
+      <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
+        </div>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
