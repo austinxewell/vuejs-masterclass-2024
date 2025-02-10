d46e359668ded1bae29fa4e802831ed5f1c75a13
Austin Ewell <austin.ewell86@gmail.com>
2025-02-09 21:45:23 -0700

    fixed sizing on sidebar and moved add task/project to the topnav component

--- src/components/Layout/main/AuthLayout.vue
+++ src/components/Layout/main/AuthLayout.vue
@@ -30,7 +30,7 @@ onUnmounted(() => {
 
 const containerMargin = computed(() => {
   if (isMobile.value) return 'ml-0'
-  return menuOpen.value ? 'ml-52' : 'ml-24'
+  return menuOpen.value ? 'ml-52' : 'ml-14'
 })
 
 provide(menuKey, {
@@ -41,18 +41,14 @@ provide(menuKey, {
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <AppSidebar
-      v-if="!isMobile"
-      @task-clicked="taskSheetOpen = true"
-      @project-clicked="projectSheetOpen = true"
-    />
-    <MobileNav v-if="isMobile" />
+    <AppSidebar v-if="!isMobile" />
+    <MobileNav v-else />
 
     <AppNewTask v-model="taskSheetOpen" />
     <AppNewProject v-model="projectSheetOpen" />
 
     <div class="flex flex-1 flex-col transition-[margin]" :class="containerMargin">
-      <TopNavbar />
+      <TopNavbar @task-clicked="taskSheetOpen = true" @project-clicked="projectSheetOpen = true" />
       <main class="flex-1 overflow-auto p-4 lg:gap-6 lg:p-6 pb-24">
         <div class="flex items-center pb-2 lg:pb-4">
           <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
